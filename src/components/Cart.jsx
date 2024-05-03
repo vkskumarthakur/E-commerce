3f0f28712b96b345bf493a96e27ff0e880c3495e
vikas <vkskumarthakur@gmail.com>
2024-05-03 18:13:19 +0530

    no items found in cart text updated

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Cart = () => {
   return (
     <div className="px-8 p-2">
-      <p className="text-center">no items are there in the cart</p>
+      <p className="text-center">no items are here in the cart</p>
     </div>
   );
 };
